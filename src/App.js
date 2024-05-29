c21b046082b7aa4817c7dfbfe7d11af3de73a8b3
kartikjindal <kartikjindal58.com>
2024-05-30 01:06:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  function handlePlay(arr)
+  {
+    setHistory([...history,arr]);
+    setXIsNext(!xIsNext);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Board arr={currentSquares} xIsNext={xIsNext} setXIsNext={setXIsNext} onPlay={handlePlay}/>
     </div>
   );
 }
 
-export default App;
+
+function Board({ arr,xIsNext ,onPlay}) {
+  const winner = checkWinner();
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  function checkWinner()
+     {
+      for(let i=0;i<win.length;i++){
+      if (
+        arr[win[i][0]] === arr[win[i][1]] &&
+        arr[win[i][0]] === arr[win[i][2]]
+      ) {
+        return arr[win[i][0]];
+        // console.log(arr[win[i][0]]);
+      }
+    }
+
+  }
+  function handleclick(i) {
+    let newarr = arr.slice();
+    if (arr[i] || checkWinner()) return;
+    if (xIsNext) {
+      newarr[i] = "X";
+      // setXIsNext(false);
+    } else {
+      newarr[i] = "O";
+      // setXIsNext(true);
+    }
+    onPlay(newarr);
+    // checkWinner();
+  }
+  
+  return (
+    <div>
+      <p> {status}</p>
+      <div>
+        <Square
+          value={arr[0]}
+          onsquareclick={() => {
+            handleclick(0);
+          }}
+        />
+        <Square
+          value={arr[1]}
+          onsquareclick={() => {
+            handleclick(1);
+          }}
+        />
+        <Square
+          value={arr[2]}
+          onsquareclick={() => {
+            handleclick(2);
+          }}
+        />
+      </div>
+      <div>
+        <Square
+          value={arr[3]}
+          onsquareclick={() => {
+            handleclick(3);
+          }}
+        />
+        <Square
+          value={arr[4]}
+          onsquareclick={() => {
+            handleclick(4);
+          }}
+        />
+        <Square
+          value={arr[5]}
+          onsquareclick={() => {
+            handleclick(5);
+          }}
+        />
+      </div>
+      <div>
+        <Square
+          value={arr[6]}
+          onsquareclick={() => {
+            handleclick(6);
+          }}
+        />
+        <Square
+          value={arr[7]}
+          onsquareclick={() => {
+            handleclick(7);
+          }}
+        />
+        <Square
+          value={arr[8]}
+          onsquareclick={() => {
+            handleclick(8);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+function Square({ value, onsquareclick }) {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        onsquareclick();
+      }}
+    >
+      {value}
+    </button>
+  );
+}
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
